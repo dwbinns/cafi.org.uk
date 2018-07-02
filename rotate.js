db577a08777f709f2794b37517fde8ce5258bf14
David Binns <dwbinnspublic+git@gmail.com>
2018-07-02 12:46:41 +0100

    Less flashing on carousel

--- rotate.js
+++ rotate.js
@@ -25,13 +25,12 @@ function rotate(container) {
         if (currentImage) currentImage.className='hide';
         currentImage=nextImage;
         currentImage.className='show';
-        nextImage=images[(imageNumber++)%images.length];
+        nextImage=images[(++imageNumber)%images.length];
         nextImage.style.backgroundImage='url('+["images",size, nextImage.getAttribute('data-src')].join("/")+')';
         nextImage.className='hide';
         if (title) title.textContent=currentImage.getAttribute('title');
     }
 
-    change();
     change();
 
     container.onclick=function() {paused=true;change();};
